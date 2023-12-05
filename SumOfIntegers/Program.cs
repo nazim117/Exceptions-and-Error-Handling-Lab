857db8291f1aca2d7ac793e817b5c61734c06f1f
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-12-05 17:12:55 +0100

    Add files via upload

---
+++ SumOfIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+2147483649 2 3.4 5 invalid 24 -4
+
+9876 string 10 -2147483649 -8 3 4.86555 8
+*/
+
+string input = Console.ReadLine();
+
+string[] elements = input.Split(' ');
+
+int sum = 0;
+
+foreach (string element in elements)
+{
+    try
+    {
+        int num = int.Parse(element);
+        sum += num;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+    }
+    finally
+    {
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
